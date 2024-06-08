03d70ebf4e2d68339f6afb4c0392e427fcf5d4eb
Felipe <felipe.negredo@edu.univali.br>
2024-06-07 22:19:23 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,7 +12,10 @@ def main():
         user_command = input()
         if user_command not in valid_commands:
             sys.stdout.write(f"{user_command}: command not found\n")
-
+            sys.stdout.flush()
+        else:
+            if user_command == "exit 0":
+                sys.exit(0)
 
 if __name__ == "__main__":
     main()
