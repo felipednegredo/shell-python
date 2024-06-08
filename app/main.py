0396a0caebdfad048e4a587b36d13489408ccf0a
Felipe <felipe.negredo@edu.univali.br>
2024-06-07 22:27:02 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 
 
 # noinspection PyUnreachableCode
-def main(user_commmand=None):
+def main():
 
     valid_commands = []
 
@@ -10,16 +10,16 @@ def main(user_commmand=None):
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        user_command = input()
-        if user_command == "exit 0":
-            break
-            sys.exit(0)
-        elif user_commmand.startswith("echo"):
-            sys.stdout.write(user_command[5:] + "\n")
-            sys.stdout.flush()
-        if user_command not in valid_commands:
-            sys.stdout.write(f"{user_command}: command not found\n")
-            sys.stdout.flush()
+        if user_command := input().strip():
+            if user_command == "exit 0":
+                break
+                sys.exit(0)
+            elif user_command.startswith("echo"):
+                sys.stdout.write(user_command[5:] + "\n")
+                sys.stdout.flush()
+            if user_command not in valid_commands:
+                sys.stdout.write(f"{user_command}: command not found\n")
+                sys.stdout.flush()
 
 
 if __name__ == "__main__":
